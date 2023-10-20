96813579875cba2ab58e6a9b8dccbb27fe162578
Pedro Soares Dias <pedroferraridias@gmail.com>
2023-10-20 07:33:49 -0300

    Adicionando uma escala com o hover nas tarefas e nos botões

--- src/app/editarTarefa/[id]/page.tsx
+++ src/app/editarTarefa/[id]/page.tsx
@@ -25,7 +25,7 @@ export default async function EditarTarefa({ params }) {
     return (
         <>
             <div className= "flex justify-center" >
-                <Link className="bg-blue-600 p-3 mt-6 text-center text-lg font-semibold text-white w-11/12 md:w-9/12 sm:w-3/5 rounded-md hover:bg-blue-700 transition-all duration-300" href={"/"} > Retornar a página principal </Link>
+                <Link className="bg-blue-600 p-3 mt-6 text-center text-lg font-semibold text-white w-11/12 md:w-9/12 sm:w-3/5 rounded-md hover:bg-blue-700 transition-all hover:scale-105 duration-200" href={"/"} > Retornar a página principal </Link>
             </div>
             <FormEditarTarefa id={id} titulo={titulo} descricao={descricao} />
         </>
